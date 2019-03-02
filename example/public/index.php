1ffa4527d74f7d067bb6dad9a555d2bfa44c8382
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-03-02 17:10:27 +1100

    Added landing page

--- example/public/index.php
+++ example/public/index.php
@@ -1,4 +1,28 @@
-<?php
-
-echo "Hello, world!";
-
+<?php 
+$details = explode('+', phpversion()); 
+?> 
+<html>
+<head>
+    <title>Docker PHP</title>
+    <style>
+        body {
+	    font-family: helvetica, sans-serif;
+            color: #333;
+	}
+	.details {
+            position: absolute;
+	    text-align: center;
+	    top: 50%;
+            width: 100%;
+            margin-top: -10px;
+	    font-size: 20px;
+            font-weight: 600;
+        }
+    </style>
+</head>
+<body>
+    <div class="details">
+        Using PHP <?php echo $details[0]; ?>
+    </div>
+</body>
+</html>
